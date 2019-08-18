fbfd275e75f65e2b3f207260f1841bc989a38b5d
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-08-17 21:04:35 -0700

    one taste function added

---
+++ lesson_4/js/app.js
@@ -0,0 +1,106 @@
+function App() {
+
+  var cuisineNames = [
+    {
+      type: "Japanese",
+      name: "sushi",
+      taste: "great",
+      ingredient: "fish and riceball stuffing",
+      calorie: 200,
+      price: 9.95
+    },
+
+    {
+      type: "Chinese",
+      name: "Dumpling",
+      taste: "good",
+      ingredient: "wheat and stuffing",
+      calorie: 400,
+      price: 8.50
+    },
+
+    {
+      name: "Indian",
+      name: "Curry",
+      taste: "Great",
+      ingredient: "various",
+      calorie: 500,
+      price: 12.20
+    },
+
+    {
+      name: "French",
+      name: "Croissant",
+      taste: "good",
+      ingredient: "wheat flour and butter",
+      calorie: 250,
+      price: 4.23
+    },
+
+    {
+      name: "vietnamese",
+      name: "Pho",
+      taste: "Great",
+      ingredient: "noodle soup",
+      calorie: 300,
+      price: 11.50
+    },
+
+    {
+      name: "Thai",
+      name: "Pad-thai",
+      taste: "Spicy",
+      ingredient: "spicy noodle",
+      calorie: 600,
+      price: 11.50
+    },
+
+    {
+      name: "Italian",
+      name: "Pizza",
+      taste: "Yum",
+      ingredient: "wheat base with various topping",
+      calorie: 800,
+      price: 15.30
+    },
+
+    {
+      name: "American",
+      name: "Burger",
+      taste: "moderate",
+      ingredient: "bread buns and topping",
+      calorie: 1000,
+      price: 5.50
+    },
+
+    {
+      name: "Canadian",
+      name: "Poutine",
+      taste: "Great",
+      ingredient: "potato fries and curry soup",
+      calorie: 1500,
+      price: 11.50
+    },
+
+    {
+      name: "Mexican",
+      name: "Taco",
+      taste: "good",
+      ingredient: "wheat brackets with veggies and fries",
+      calorie: 780,
+      price: 10.50
+    }
+  ];
+
+  function goodFoodSelect(arrayInput, taste) {
+    var goodFoodList = [];
+    for (var i = 0; i < arrayInput.length; i++) {
+      if (arrayInput[i].taste.toLowerCase() === taste.toLowerCase()) {
+        goodFoodList.push(arrayInput[i]);
+      }
+    }
+    return goodFoodList;
+  }
+  console.log(goodFoodSelect(cuisineNames, "great"));
+
+}
