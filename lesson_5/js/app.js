814b6123012213af3e9a77519055be35728d5b44
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-08-18 22:07:50 -0700

    higher order function filter learnt

--- lesson_5/js/app.js
+++ lesson_5/js/app.js
@@ -1,3 +1,32 @@
-function App(){
-  
+function App() {
+
+  function evaluatePrice(dress) {
+    return (dress.price > 100 && dress.price < 500);
+  }
+
+  function evaluateColors(dress) {
+    for (var i = 0; i < dress.color.length; i++) {
+      if (dress.color[i] == "black" || dress.color[i] == "white") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // myFilter is a higher order function since it takes another function and operates
+  function myFilter(inputArray, fn) {
+    var output = [];
+    for (var i = 0; i < inputArray.length; i++) {
+      if (fn(inputArray[i])) {
+        output.push(inputArray[i]);
+      }
+    }
+    return output;
+  }
+
+  console.log(myFilter(dresses, evaluateColors));
+  console.log(myFilter(dresses, evaluatePrice));
+
+  console.log(dresses.filter(evaluateColors));
+  console.log(dresses.filter(evaluatePrice));
 }
---
+++ lesson_5/js/data.js
@@ -0,0 +1,82 @@
+var dresses = [
+  {
+    dress: "skirt",
+    type: "cotton",
+    price: 32,
+    rating: "good",
+    color: ["white", "pink", "purple", "blue"],
+  },
+
+  {
+    dress: "trousers",
+    type: "cotton",
+    price: 200,
+    rating: "good",
+    color: ["blue", "white", "red", "green"]
+  },
+
+  {
+    dress: "slacks",
+    type: "polyester",
+    price: 45,
+    rating: "great",
+    color: ["blue", "purple", "navy", "green"]
+  },
+
+  {
+    dress: "long skirt",
+    type: "rayon",
+    price: 40,
+    rating: "great",
+    color: ["black", "yellow", "navy", "coral"]
+  },
+
+  {
+    dress: "shirt",
+    type: "cotton",
+    price: 45,
+    rating: "okay",
+    color: ["black", "yellow", "green", "coral"]
+  },
+
+  {
+    dress: "top",
+    type: "rayon",
+    price: 100,
+    rating: "okay",
+    color: ["black", "yellow", "white", "mint"]
+  },
+
+  {
+    dress: "shorts",
+    type: "jeans",
+    price: 150,
+    rating: "great",
+    color: ["purple", "denim", "white", "mint"]
+  },
+
+  {
+    dress: "socks",
+    type: "cotton",
+    price: 32,
+    rating: "good",
+    color: ["pink", "grey", "white", "mint"]
+  },
+
+  {
+    dress: "jacket",
+    type: "jeans",
+    price: 400,
+    rating: "okay",
+    color: ["pink", "brown", "navy", "peach"]
+  },
+
+  {
+    dress: "dress",
+    type: "cotton and rayon",
+    price: 230,
+    rating: "great",
+    color: ["pink", "brown", "navy", "peach"]
+  }
+
+]
