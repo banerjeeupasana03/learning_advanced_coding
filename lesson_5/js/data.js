dd130c19432e7b08f295ac11d64bf1706b5f3ec1
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-08-24 19:24:33 -0700

    new json file added in data

--- lesson_5/js/data.js
+++ lesson_5/js/data.js
@@ -78,5 +78,103 @@ var dresses = [
     rating: "great",
     color: ["pink", "brown", "navy", "peach"]
   }
+]
 
+var food = [
+  {
+    country: "India",
+    continenet: "Southeast Asia",
+    food: "Curry",
+    calorie: 200,
+    price: 14.67,
+    image: "https://image.shutterstock.com/image-photo/indian-curry-chicken-vindaloo-balti-260nw-223515667.jpg"
+  },
+  {
+    country: "Japan",
+    continenet: "Asia",
+    food: "Sushi",
+    calorie: 280,
+    price: 13.67,
+    image: "https://www.matchingfoodandwine.com/files/blogattachments/top/sushi-1522744937-0.jpg"
+  },
+  {
+    country: "China",
+    continenet: "Asia",
+    food: "noodle soup",
+    calorie: 400,
+    price: 16.67,
+    image: "https://www.goodfood.com.au/content/dam/images/g/x/m/e/2/s/image.related.wideLandscape.940x529.gxreh5.png/1529395353932.jpg"
+  },
+  {
+    country: "France",
+    continenet: "Europe",
+    food: "French-Toast",
+    calorie: 500,
+    price: 12.67,
+    image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2008/3/26/0/IE0309_French-Toast.jpg.rend.hgtvcom.826.620.suffix/1431730431340.jpeg"
+  },
+  {
+    country: "Turkey",
+    continenet: "Middle East",
+    food: "Baklava",
+    calorie: 334,
+    price: 18.67,
+    image: "https://www.thespruceeats.com/thmb/5eB0XVQn5sKRloo3-tbxbC6NCY4=/2832x1914/filters:fill(auto,1)/PistachioBaklava-GettyImages-183422455-5997abb09abed50010b57402.jpg"
+  },
+  {
+    country: "Greece",
+    continenet: "Europe",
+    food: "Souvlaki",
+    calorie: 310,
+    price: 20.54,
+    image: "https://media.chefdehome.com/750/0/0/chicken-souvlaki/greek-chicken-souvlaki.jpg"
+  },
+  {
+    country: "Canada",
+    continenet: "North America",
+    food: "Poutine",
+    calorie: 1110,
+    price: 21.67,
+    image: "https://d1doqjmisr497k.cloudfront.net/-/media/mccormickcan/recipes/desktop/ultimate_canadian_poutine_1144_2000x1125.jpg?vd=20180617T021804Z&hash=719DEDE6EB66C17B9475190C25DCB8E92995726A"
+  },
+  {
+    country: "America",
+    continenet: "North America",
+    food: "chicken burger and fries",
+    calorie: 650,
+    price: 10.67,
+    image: "https://previews.123rf.com/images/annapustynnikova/annapustynnikova1703/annapustynnikova170300217/73545064-burger-hamburger-with-french-fries-cutting-board-.jpg"
+  },
+  {
+    country: "Spain",
+    continenet: "Europe",
+    food: "paella",
+    calorie: 510,
+    price: 25.45,
+    image: "https://realfood.tesco.com/media/images/RFO-1400x919-ClassicPaella-a00aa895-f133-4f2b-a009-d17eb65c34a5-0-1400x919.jpg"
+  },
+  {
+    country: "Mexico",
+    continenet: "South America",
+    food: "taco",
+    calorie: 400,
+    price: 13.67,
+    image: "https://img.taste.com.au/w_-0kcUJ/taste/2016/11/aussie-style-beef-and-salad-tacos-86525-1.jpeg"
+  },
+  {
+    country: "Vietnam",
+    continenet: "Asia",
+    food: "pho",
+    calorie: 300,
+    price: 13.67,
+    image: "https://www.simplyrecipes.com/wp-content/uploads/2017/02/2017-02-07-ChickenPho-14.jpg"
+  },
+  {
+    country: "Morocco",
+    continenet: "Africa",
+    food: "tagine",
+    calorie: 800,
+    price: 30.67,
+    image: "https://www.diabetes.org.uk/resources-s3/migration/recipes/Vegetable-And-Chickpea-Tagine.jpg"
+  }
 ]
